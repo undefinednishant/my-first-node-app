9054ec7c819dde75601c50508f1d26fff9e1a3e6
Nishant Kumar <knishant086@gmail.com>
2021-02-16 19:45:54 +0530

    implemented update functionality

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const {
+    response
+} = require('express');
 
 const app = express();
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
+
+// middleware to parse application/json from body request
+app.use(bodyParser({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 // connect mongo
-mongoose.connect('mongodb://localhost:27017/my-test', { useNewUrlParser: true ,  useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/my-test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 
-// schema - model
+// player schema - model
 const playerSchema = new Schema({
     name: String,
     age: Number
@@ -16,15 +30,106 @@ const playerSchema = new Schema({
 
 const playerModel = mongoose.model('player', playerSchema);
 
+// create player
+app.post('/player', (req, res) => {
+    console.log(' h ', req.body)
+    const playerData = new playerModel({
+        name: req.body.name,
+        age: req.body.age
+    });
 
-const playerData = new playerModel({
-    name: 'rahman',
-    age: 20
+    playerData.save(() => {
+        console.log('data added');
+        res.json({
+            message: 'Player has been added'
+        })
+    });
+
 });
 
-playerData.save(() => {
-    console.log('data added');
+
+// get all players
+app.get('/players', async (req, res) => {
+    const players = await playerModel.find();
+    res.json(players);
+});
+
+// get single player
+app.get('/player/:id', async (req, res) => {
+    const id = req.params.id;
+    const isValidId = mongoose.Types.ObjectId.isValid(req.params.id);
+    if (isValidId) {
+        const playerResult = await playerModel.findOne({
+            "_id": id
+        });
+
+        if (!playerResult) {
+            res.status(404).json({
+                message: 'This player is not exist'
+            });
+        } else {
+            res.json(playerResult);
+        }
+
+    } else {
+        res.status(400).json({
+            message: 'This is not a valid id'
+        });
+    }
+
 })
+
+
+// update player
+app.put('/player/:id', async (req, res) => {
+    const id = req.params.id;
+    const isValidId = mongoose.Types.ObjectId.isValid(req.params.id);
+    if (isValidId) {
+        const playerResult = await playerModel.findOne({
+            "_id": id
+        });
+
+        if (!playerResult) {
+            res.status(404).json({
+                message: 'This player is not exist'
+            });
+        } else {
+
+            // update data
+            const playerData = {
+                name: req.body.name,
+                age: req.body.age
+            };
+            
+            // write update suff
+            const query = { _id: id };
+            const updatedPlayer = await playerModel.updateOne(query, playerData);
+            
+            // show response
+            res.status(200).json({
+                id: id,
+                message: 'Updated',
+                info: {
+                    matched: updatedPlayer.n,
+                    modified: updatedPlayer.nModified
+                }
+            });
+
+        }
+
+    } else {
+        res.status(400).json({
+            message: 'This is not a valid id'
+        });
+    }
+});
+
+
+
+
+
+
+
 
 
 
